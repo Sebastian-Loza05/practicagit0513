fed3f99849ed0225c3a96cbf012558618871b7c8
Luis-Golac <luis.golac@utec.edu.pe>
2023-08-17 20:41:21 -0500

    multiplicar

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 
 def hello():
     return '<h1>Hello, World dos!</h1>'
+
+@app.route('/multiplicar/<param1>/<param2>')
+def multiplicar(param1, param2):
+    valor = int(param1) * int(param2)
+    return str(valor)
